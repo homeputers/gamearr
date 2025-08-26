1068e31f72fdea1bba149e8a833412d09bff969c
Andres Rovira <wrovira@paypal.com>
2025-08-25 22:43:54 -0600

    feat(api): scaffold nestjs app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,14 @@
-import { config, logger } from '@gamearr/shared';
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import pinoHttp from 'pino-http';
+import { logger, withRequestId } from '@gamearr/shared';
+import { AppModule } from './app.module';
 
-logger.info('api start');
-console.log(config.dbUrl);
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(pinoHttp({ logger }));
+  app.use((req, _res, next) => withRequestId(() => next(), (req as any).id));
+  await app.listen(3000);
+}
+
+bootstrap();
