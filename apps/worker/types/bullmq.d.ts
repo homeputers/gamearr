c477522c5ed39cca0fd734399d95fd8085ef9baf
Andres Rovira <wrovira@paypal.com>
2025-08-26 17:34:42 -0600

    fix: compile worker scan

--- apps/worker/types/bullmq.d.ts
+++ apps/worker/types/bullmq.d.ts
@@ -2,6 +2,7 @@ declare module 'bullmq' {
   export class Queue {
     name: string;
     constructor(name: string, opts?: any);
+    add(name: string, data: any, opts?: any): Promise<any>;
   }
   export class Worker {
     constructor(name: string, processor: any, opts?: any);
